0e54b39fc027e5db9e31bc8f9e56e7e2ec15034c
Andres Arevalo <andresfelipear@gmail.com>
2021-11-04 13:02:21 -0500

    solution challenge1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import ChallengeOne from "./Components/ChallengeOne";
 
 function App() {
   return (
     <div className="App">
       <h1>React Lab 101</h1>
+      <ChallengeOne/>
       {/* Import Components (ChallengeOne and Challenge Two) here */}
     </div>
   );
--- src/Components/ChallengeOne.jsx
+++ src/Components/ChallengeOne.jsx
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
+import image1 from'../Assets/look-left.jpeg'
+import image2 from'../Assets/look-right.jpeg'
 //import images
 
 class ChallengeOne extends Component {
   //declare the state here
   state = {
+    image: image1
+
   };
 
+
   //click left/right button handler goes here
+  clickLeft = ()=>{
+    this.setState({
+      image:image1
+    })
+  }
+
+  clickRight = ()=>{
+    this.setState({
+      image:image2
+    })
+  }
 
   render() {
     return (
@@ -15,12 +31,12 @@ class ChallengeOne extends Component {
         <div className="msg">
           <img
             className="ch1"
-            src=""
+            src={this.state.image}
             alt=""
           />
         </div>
-        <button className="btn">⭠</button>
-        <button className="btn">⭢</button>
+        <button className="btn" onClick={this.clickLeft}>⭠</button>
+        <button className="btn" onClick={this.clickRight}>⭢</button>
       </>
     );
   }
